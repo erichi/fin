4fac573a51601bf895950f5a2b151bf8a2d44943
Rainum <rainum.ua@gmail.com>
2013-02-09 12:24:51 +0200

    cashflow fixes

--- apps/frontend/modules/business_unit/templates/cashflowSuccess.php
+++ apps/frontend/modules/business_unit/templates/cashflowSuccess.php
@@ -1,82 +1,88 @@
 <?php use_helper('I18N')?>
 <?php //use_stylesheet('default.css') ?>
 <?php use_stylesheet('smoothness/jquery-ui-1.8.16.custom.css') ?>
+<?php use_stylesheet('main.css') ?>
 <?php use_javascript('jquery-ui-1.8.16.custom.min.js') ?>
+<?php use_javascript('jquery.scrollTo.js') ?>
 <?php use_javascript('cashflow.js') ?>
 <?php use_javascript('jquery.ui.datepicker-ru.js')?>
 
-<style>
-#cashflow_container ul li
-{
-  padding: 0px;
-  list-style: none;
-}
-
-.income_payments li{
-	color: green;
-}
-.outcome_payments li{
-	color: red;
-}
-
-</style>
-
-
 <div id="cashflow_container" class="cashflow">
-	<h1>Cashflow</h1>
-		<hr/>
-	<div id="sf_admin_content">
-		<form action="<?php echo url_for('@cashflow')?>" method="POST">
-			<input type="hidden" name="id" value="<?php echo $sf_request->getParameter('id'); ?>">
-			<input type="text" name="date" id="cashflow_start_date" value="<?php echo date('Y-m-d'); ?>">
-			<button class="btn" type="submit">Обновить по дате</button>
-		</form>
-		<div>
-			<table class="table table-bordered">
+    <h1>Cashflow</h1>
+    <hr>
+    <div id="sf_admin_content">
+        <form action="<?php echo url_for('@cashflow')?>" method="POST">
+            <input type="hidden" name="id" value="<?php echo $sf_request->getParameter('id'); ?>">
+            <input type="text" name="date" id="cashflow_start_date" value="<?php echo date('Y-m-d'); ?>">
+        </form>
 
-				<tr>
-					<th></th>
-					<?php foreach ($range as $r): ?>
-					<th><?php echo $r; ?></th>
-					<?php endforeach; ?>
-				</tr>
-				<?php foreach ($result as $res): ?>
-				<tr>
-					<td><?php echo $res['name']; ?></td>
-					<?php foreach ($res['dates'] as $date): ?>
-						<?php if(!empty($date)):?>
-						<td>
-							<?php if(isset($date['in'])): ?>
-							<ul class="income_payments">	
-								<?php foreach ($date['in'] as $amount): ?>
-								<li><?php echo $amount; ?></li>
-								<?php endforeach; ?>
-							</ul>
-							<?php endif; ?>
-							<?php if(isset($date['out'])): ?>
-							<ul class="outcome_payments">	
-								<?php foreach ($date['out'] as $amount): ?>
-								<li><?php echo $amount; ?></li>
-								<?php endforeach; ?>
-							</ul>
-							<?php endif; ?>
-						</td>
+        <div class="clearfix">
+            <table id="first-col" class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>&nbsp;</th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
 
-						<?php endif; ?>
-					<?php endforeach; ?>
-				</tr>
-				<?php endforeach; ?>
+            <table id="last-col" class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>&nbsp;</th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
 
-			</table>
-		</div>
-	</div>
+            <div class="scrollable-table-viewport">
+                <div class="scrollable-table">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <?php foreach ($range as $r): ?>
+                                <th id="<?php echo $r; ?>"><?php echo $r; ?></th>
+                                <?php endforeach; ?>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($result as $res): ?>
+                            <tr>
+                                <td><?php echo $res['name']; ?></td>
+                                <?php foreach ($res['dates'] as $date): ?>
+                                <?php if(!empty($date)):?>
+                                <td>
+                                    <?php if(isset($date['in'])): ?>
+                                    <ul class="income_payments">
+                                        <?php foreach ($date['in'] as $amount): ?>
+                                        <li><?php echo $amount; ?></li>
+                                        <?php endforeach; ?>
+                                    </ul>
+                                    <?php endif; ?>
+                                    <?php if(isset($date['out'])): ?>
+                                    <ul class="outcome_payments">
+                                        <?php foreach ($date['out'] as $amount): ?>
+                                        <li><?php echo $amount; ?></li>
+                                        <?php endforeach; ?>
+                                    </ul>
+                                    <?php endif; ?>
+                                </td>
+
+                                <?php endif; ?>
+                                <?php endforeach; ?>
+                            </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <?php /*
 
 <script type="text/javascript">
-	var cashflow_index_url = "<?php echo url_for('business_unit/cashflow?id='.$business_unit_id.'&date='.$date) ?>";
-	var approve_income_payment_url = "<?php echo url_for('business_unit/approveIncomePayment') ?>";
-	var approve_outcome_payment_url = "<?php echo url_for('business_unit/approveOutcomePayment') ?>";
-</script>
-*/
-?>
+    var cashflow_index_url = "<?php echo url_for('business_unit/cashflow?id='.$business_unit_id.'&date='.$date) ?>";
+    var approve_income_payment_url = "<?php echo url_for('business_unit/approveIncomePayment') ?>";
+    var approve_outcome_payment_url = "<?php echo url_for('business_unit/approveOutcomePayment') ?>";
+</script> */ ?>
--- web/js/cashflow.js
+++ web/js/cashflow.js
@@ -1,31 +1,99 @@
-$(function(){
-	
-	$('#cashflow_start_date').datepicker({
-		firstDay: 1,
-	});
-	
+$(function () {
+    // Вылючил выходные в календаре
+    // Добавил два фиксированных столбца, которые не скролятся
+    // Сделал прокрутку таблицы к выбранной дате
+    // Правки верстки, стилей
+    // Сделал единый ховер для всех таблиц
+
+
+    // Collect column cells and build edge rows
+    function buildEdgeCells(cells) {
+        var colCells = [],
+            newColRows = [];
+
+        cells.each(function (i, el) {
+            colCells.push({
+                text: $(el).text(),
+                height: $.browser.opera ? $(el).outerHeight() : $(el).height()
+            });
+
+            $(el).remove();
+        });
+
+        $.each(colCells, function (i, cell) {
+            var row = '<tr><td style="height: ' + cell.height + 'px;">' + cell.text + '</td></tr>'
+            newColRows.push(row)
+        });
+
+        return newColRows.join('');
+    }
+
+    var viewport = $('.scrollable-table-viewport'),
+        wrap = viewport.find('.scrollable-table'),
+        table = wrap.find('table'),
+        cells = table.find('thead').find('th'),
+        totalWidth = 0;
+
+    // Set proper table width
+    cells.each(function (i, el) {
+        totalWidth += $(el).outerWidth();
+    });
+
+    wrap.width(totalWidth);
+
+    // Build edge columns
+    $('#first-col').find('tbody').append(
+        buildEdgeCells(table.find('tr').find('td:first'))
+    );
+
+    $('#last-col').find('tbody').append(
+        buildEdgeCells(table.find('tr').find('td:last'))
+    );
+
+    $('.table tbody').find('tr').hover(
+        function() {
+            var index = $(this).index();
+            $('.table tbody').find('tr:eq(' + index + ')').addClass('hover');
+        }, function() {
+            var index = $(this).index();
+            $('.table tbody').find('tr:eq(' + index + ')').removeClass('hover');
+        }
+    );
+
+    $('#cashflow_start_date').datepicker({
+        beforeShowDay: function (date) {
+            return [date.getDay() > 0 && date.getDay() < 6, '']; // Disable weekends
+        },
+        firstDay: 1,
+        dateFormat: 'yy-mm-dd',
+        onSelect: function (date) {
+            var col = $('#' + date);
+
+            if (col.length) {
+                viewport.scrollTo(col.position().left + 2, 0, { duration: 700 });
+            }
+        }
+    });
 });
 
-function approveJoIp(id)
-{
-	$.ajax({
-		url: approve_income_payment_url,
-		async: false,
-		data: 'income_payment_id='+id,
-		success: function(){
-			window.location = cashflow_index_url;
-		}
-	});
+function approveJoIp(id) {
+    $.ajax({
+        url: approve_income_payment_url,
+        async: false,
+        data: 'income_payment_id=' + id,
+        success: function () {
+            window.location = cashflow_index_url;
+        }
+    });
 }
 
-function approveJoOp(id)
-{
-	$.ajax({
-		url: approve_outcome_payment_url,
-		async: false,
-		data: 'outcome_payment_id='+id,
-		success: function(){
-			window.location = cashflow_index_url;
-		}
-	});
+function approveJoOp(id) {
+    $.ajax({
+        url: approve_outcome_payment_url,
+        async: false,
+        data: 'outcome_payment_id=' + id,
+        success: function () {
+            window.location = cashflow_index_url;
+        }
+    });
 }
