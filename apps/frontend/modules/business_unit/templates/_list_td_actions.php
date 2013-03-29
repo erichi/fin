cacb7f4da93298a5a2ba17a188892517d10123e4
fingerv <fingerv@gmail.com>
2013-03-29 15:04:01 +0200

    delete BU CSRF generation fix

--- apps/frontend/modules/business_unit/templates/_list_td_actions.php
+++ apps/frontend/modules/business_unit/templates/_list_td_actions.php
@@ -1,3 +1,7 @@
+<?php
+	$f = new BaseForm();
+	$csrf = $f->getCSRFToken();
+?>
 <td>
 <div class="btn-group">
           <a class="btn" href="#"><i class="icon-briefcase icon-black"></i> BU</a>
@@ -8,8 +12,7 @@
             <li><a href="/business_unit/cashflow?id=<?php echo $BusinessUnit->getId(); ?>"><i class="icon-list-alt"></i> Cashflow</a></li>
             <li class="divider"></li>
             <li><a href="/business_unit/<?php echo $BusinessUnit->getId(); ?>/edit"><i class="icon-pencil"></i> Редактировать</a></li>
-            <li><a onclick="if (confirm('Are you sure?')) { var f = document.createElement('form'); f.style.display = 'none'; this.parentNode.appendChild(f); f.method = 'post'; f.action = this.href;var m = document.createElement('input'); m.setAttribute('type', 'hidden'); m.setAttribute('name', 'sf_method'); m.setAttribute('value', 'delete'); f.appendChild(m);var m = document.createElement('input'); m.setAttribute('type', 'hidden'); m.setAttribute('name', '_csrf_token'); m.setAttribute('value', '8dcecfa422736755928a88d4e793f922'); f.appendChild(m);f.submit(); };return false;" href="/business_unit/<?php echo $BusinessUnit->getId(); ?>"><i class="icon-trash"></i> Удалить</a></li>
+            <li><a onclick="if (confirm('Are you sure?')) { var f = document.createElement('form'); f.style.display = 'none'; this.parentNode.appendChild(f); f.method = 'post'; f.action = this.href;var m = document.createElement('input'); m.setAttribute('type', 'hidden'); m.setAttribute('name', 'sf_method'); m.setAttribute('value', 'delete'); f.appendChild(m);var m = document.createElement('input'); m.setAttribute('type', 'hidden'); m.setAttribute('name', '_csrf_token'); m.setAttribute('value', '<?php echo $csrf; ?>'); f.appendChild(m);f.submit(); };return false;" href="/business_unit/<?php echo $BusinessUnit->getId(); ?>"><i class="icon-trash"></i> Удалить</a></li>
           </ul>
         </div>
 
-    
