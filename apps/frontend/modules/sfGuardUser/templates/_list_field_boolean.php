3c80da1cb4209814c3b534e2039d4d4a9eb4a328
fingerv <fingerv@gmail.com>
2013-03-30 17:03:35 +0200

    user list image fix

---
+++ apps/frontend/modules/sfGuardUser/templates/_list_field_boolean.php
@@ -0,0 +1,5 @@
+<?php if ($value): ?>
+  <?php echo image_tag('/sf/sf_admin/images/tick.png', array('alt' => __('Checked', array(), 'sf_admin'), 'title' => __('Checked', array(), 'sf_admin'))) ?>
+<?php else: ?>
+  &nbsp;
+<?php endif; ?>
